a0b28fc32c5359793706673f912535ac3ea568e7
JuliaGolban <julia.j.shcherban@gmail.com>
2023-01-04 12:38:31 +0200

    used fs.readFile(), fs.writeFile()

--- contacts.js
+++ contacts.js
@@ -1,21 +1,72 @@
-/*
- * Розкоментуйте і запиши значення
- * const contactsPath = ;
- */
+const fs = require('fs').promises;
+const path = require('path');
 
-// TODO: задокументувати кожну функцію
+const uuid = require('uuid');
+
+const contactsPath = path.join(__dirname, 'db/contacts.json');
+
 function listContacts() {
-  // ...твій код
+  fs.readFile(contactsPath)
+    .then(data => {
+      const contacts = JSON.parse(data.toString());
+      console.log('listContacts ~ contacts', contacts);
+      return contacts;
+    })
+    .catch(err => console.log(err.message));
 }
 
 function getContactById(contactId) {
-  // ...твій код
+  fs.readFile(contactsPath)
+    .then(data => {
+      const contacts = JSON.parse(data.toString());
+      const contact = contacts.find(contact => {
+        return contact.id === contactId;
+      });
+      console.log('getContactById ~ contact', contact);
+      return contact;
+    })
+    .catch(err => console.log('getContactById', err.message));
 }
 
 function removeContact(contactId) {
-  // ...твій код
+  fs.readFile(contactsPath)
+    .then(data => {
+      const contacts = JSON.parse(data.toString());
+      const index = contacts.findIndex(contact => contact.id === contactId);
+      const deletedContact = contacts[index];
+      if (index !== -1) {
+        contacts.splice(index, 1);
+        fs.writeFile(contactsPath, JSON.stringify(contacts));
+      }
+      console.log('removeContact ~ deletedContact', deletedContact);
+      return deletedContact;
+    })
+
+    .catch(err => console.log('removeContact', err.message));
 }
 
 function addContact(name, email, phone) {
-  // ...твій код
+  const newContact = {
+    id: uuid.v4(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  fs.readFile(contactsPath)
+    .then(data => {
+      const contacts = JSON.parse(data.toString());
+      contacts.push(newContact);
+      fs.writeFile(contactsPath, JSON.stringify(contacts));
+      console.log('addContact ~ newContact', newContact);
+      return newContact;
+    })
+
+    .catch(err => console.log('addContact', err.message));
 }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,9 @@
+const contacts = require('./contacts');
+
+contacts.listContacts();
+
+contacts.getContactById('3');
+
+// contacts.removeContact('10');
+
+// contacts.addContact('Julia', 'julia@gmail.com', '(068) 864 54 47")');
